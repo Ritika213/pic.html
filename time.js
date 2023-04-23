eccd397aaabd16f0b1990555c40afd0ba967f6be
Ritika213 <ritikakgupta6@gmail.com>
2023-04-23 13:54:30 +0530

    fast

---
+++ time.js
@@ -0,0 +1,25 @@
+var newDiv=document.createElement('div');
+
+newDiv.className='hello';
+
+newDiv.id='hello1'
+
+newDiv.setAttribute('title','Hello Div')
+
+var newDivText=document.createTextNode('Hello worlds');
+
+newDiv.appendChild(newDivText);
+
+var container =document.querySelector('header .container');
+
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+newDiv.style.fontSize = '30px';
+
+container.insertBefore(newDiv.h1);
+parentNode = document.getElementById('items')
+parentNode.appendFirstChild(helloworldlielement)
+parentNode.innerhtml='<li>item 1</li><li>item 2</li><li>item 3</li><li>item 4</li>'
+
+parentNode.innerhtml = '<li>Hello world</li>' + parentNode.innerhtml
